a9c57c774395613a3499fb39aa6c5ec50e3d0f2d
Sabah Khan <skhan@LT-C02XD0HXJG5M.local>
2019-08-07 12:35:19 -0400

    adding styling for textarea

--- client/src/Components/templateCreation/index.jsx
+++ client/src/Components/templateCreation/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Dropdown from './dropdown.jsx';
-import Button from './button.jsx'
-import './style.css';
-import * as stubData from './stubData.json'
+import React from "react";
+import Dropdown from "./dropdown.jsx";
+import Button from "./button.jsx";
+import "./style.css";
+import * as stubData from "./stubData.json";
 
 export default class TemplateCreation extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = Object.assign({}, props, stubData);
   }
 
@@ -14,27 +14,27 @@ export default class TemplateCreation extends React.Component {
     return (
       <form>
         <section id="template-header">
-            <input placeholder="Template Name *Required" required />
-            <input placeholder="Template Description *Required" required />
+          <input placeholder="Template Name *Required" required />
+          <input placeholder="Template Description *Required" required />
         </section>
         <section id="template-config">
-          <Dropdown name="service" data={stubData.services}/>
-          <Dropdown name="environemt" data={stubData.environment}/>
-          <Dropdown name="configuration" data={stubData.configuration}/>
+          <Dropdown name="service" data={stubData.services} />
+          <Dropdown name="environemt" data={stubData.environment} />
+          <Dropdown name="configuration" data={stubData.configuration} />
           <Dropdown name="configMethod" />
         </section>
-        <section id="template-builder">
+        <section id="template-builderHeader">
           <Dropdown name="method" />
-          <input className="template-url"/ >
-          <div className="template-builderBody">
-            <textarea className="template-header"></textarea>
-            <textarea className="template-body"></textarea>
-          </div>
+          <input className="template-url" />
+        </section>
+        <section id="template-builderBody">
+          <textarea className="template-header" />
+          <textarea className="template-body" />
         </section>
         <section id="template-button">
           <Button />
         </section>
       </form>
-    )
+    );
   }
 }
