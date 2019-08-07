d9d0e5ca02f436d29ab264a3aaef2dc514cc97ab
Christopher MCAdams <cmcadams@c.systemsinmotion.com>
2019-08-07 11:34:16 -0400

    app layout BareBones

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,33 @@
 import React from 'react';
 import './App.css';
-import {TemplateCreation, CardComponent} from './Components'
+import {CardComponent, Leftnav} from './Components'
 
-// holding for reference - delete before merging
-// function displayComponent(props) {
-//   return (
-//     <section className="component"> <h3>{props.name}</h3> </section>
-//   )
-// }
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {name: '<human>'};
-  }
-
-  componentDidMount() {
-    fetch('/test', {accept: 'application/json'})
-      .then(res => res.json())
-      .then(response => {
-        this.setState(response)
-    })
-    .catch(err => console.log(err))
+    this.state = {title: 'Template List'};
   }
 
   render () {
-
     return (
-      <div className="App">
-        <h2>
-          ASTRA Component Library
-        </h2>
+    <div className="App">
+    <Leftnav />
+      <div id="content-container">
+        <header>
+          <h2>
+      {this.state.title}
+          </h2>
+        </header>
         {/* TODO loop through exported components and add them to the page */}
         {/* Place Components below*/}
         <React.Fragment>
-          <section className="component" name="templateCreation">
-            <TemplateCreation />
-          </section>
           <section className="component" name="cardComponent">
-          <CardComponent />
-        </section>
+            <CardComponent />
+          </section>
         </React.Fragment>
-      </div>
+        </div>
+    </div>
     );
   }
 }
