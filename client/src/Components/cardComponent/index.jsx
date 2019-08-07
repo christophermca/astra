773aaf62e37e7bb3d5307276b0cd3c8e9833d633
Sabah Khan <skhan@LT-C02XD0HXJG5M.local>
2019-08-07 10:49:29 -0400

    added checkbox

--- client/src/Components/cardComponent/index.jsx
+++ client/src/Components/cardComponent/index.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./style.css";
 
 export default class CardComponent extends React.Component {
+
+  handleClick = () =>{
+    console.log("clicked");
+  }
   render() {
     return (
       <section id="card-container">
@@ -9,18 +13,24 @@ export default class CardComponent extends React.Component {
         <div className="card-button">
           execute<span>...</span>
         </div>
-        <div className="card-info">
-          <div className="card-value">
-            <span className="card-label">Service</span>value
+
+        <div className="card-main">
+          <div className="card-checkbox">
+            <input onClick={this.handleClick} type="checkBox" />
           </div>
-          <div className="card-value">
-            <span className="card-label">Created Time</span>value
-          </div>
-          <div className="card-value">
-            <span className="card-label">User ID</span>value
-          </div>
-          <div className="card-value">
-            <span className="card-label">Team Name</span>value
+          <div className="card-info">
+            <div className="card-value">
+              <span className="card-label">Service</span>value
+            </div>
+            <div className="card-value">
+              <span className="card-label">Created Time</span>value
+            </div>
+            <div className="card-value">
+              <span className="card-label">User ID</span>value
+            </div>
+            <div className="card-value">
+              <span className="card-label">Team Name</span>value
+            </div>
           </div>
         </div>
         <div className="card-detail">details</div>
