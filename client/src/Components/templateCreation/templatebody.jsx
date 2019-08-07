42eb0779dd57a14c262bf61e95339018dabe6e1a
Sabah Khan <skhan@LT-C02XD0HXJG5M.local>
2019-08-07 16:44:04 -0400

    template body completed

--- client/src/Components/templateCreation/templatebody.jsx
+++ client/src/Components/templateCreation/templatebody.jsx
@@ -1,20 +1,67 @@
 import React from "react";
 import "./style.css";
 
-const TemplateBody = () => {
-  return (
-    <section className="template-builderBody">
-      <div className="template-header">
-        <label>header</label>
-        <textarea />
-      </div>
+export default class TemplateBody extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bodyVisibility: true,
+      line: "body"
+    };
+  }
 
-      <div className="template-body">
-        <label>body RAW</label>
-        <textarea />
-      </div>
-    </section>
-  );
-};
+  handleBody = () => {
+    console.log("body clicked");
+    this.setState({ bodyVisibility: true });
+  };
 
-export default TemplateBody;
+  handleData = () => {
+    console.log("data click");
+    this.setState({ bodyVisibility: false });
+  };
+  render() {
+    return (
+      <section className="template-builderBody">
+        <div className="template-header">
+          <label>Header</label>
+          <textarea />
+        </div>
+
+        <div className="template-body">
+          <label onClick={this.handleBody}>Body RAW</label>
+          <label onClick={this.handleData}>Form Data</label>
+          {this.state.bodyVisibility && (
+            <div>
+              <textarea placeholder="BodyRAW" />
+            </div>
+          )}
+
+          {!this.state.bodyVisibility && (
+            <div>
+              <table>
+                <tr>
+                    <td></td>
+                  <td>key</td>
+                  <td>value</td>
+                  <td>description</td>
+                </tr>
+                <tr>
+                    <td><input type="checkbox"></input></td>
+                  <td>
+                    <input placeholder="key" />
+                  </td>
+                  <td>
+                    <input placeholder="value" />
+                  </td>
+                  <td>
+                    <input placeholder="description" />
+                  </td>
+                </tr>
+              </table>
+            </div>
+          )}
+        </div>
+      </section>
+    );
+  }
+}
