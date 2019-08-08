4001f0cf6d8e70190b05a32ef3e48eabf4f8a3a8
Tramane Hall <thall@LT-C02XJ0H5JG5M.local>
2019-08-08 11:07:45 -0400

    Add comment with full route url

--- server/resources/template/template.router.js
+++ server/resources/template/template.router.js
@@ -4,6 +4,7 @@ const templateCrud = require('./templateCrud.utils')
 
 const router = express.Router()
 
+// /api/templates/templateList
 router
   .route('/templatelist')
   .get(templateCrud.getAllTemplates)
