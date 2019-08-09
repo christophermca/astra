5f4e7df8c7fedcaab1a4ce39d0091836a5467062
Sabah Khan <skhan@LT-C02XD0HXJG5M.local>
2019-08-09 12:31:31 -0400

    installed redux and react-redux, store setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,51 @@
-import React from 'react';
-import './App.css';
-import {CardComponent, Leftnav, TemplateCreation} from './Components'
+import React from "react";
+import "./App.css";
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
+import rootReducers from "./reducers/index";
+import { CardComponent, Leftnav, TemplateCreation } from "./Components";
 
+const store = createStore(rootReducers);
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {title: 'Template List'};
+    this.state = { title: "Template List" };
   }
 
   componentDidMount() {
-    fetch('/api/templates/templateList', { headers: { accepts: 'application/json' } })
+    fetch("/api/templates/templateList", {
+      headers: { accepts: "application/json" }
+    })
       .then(response => response.json())
-      .then(json =>  this.setState({"list": json }))
-    }
+      .then(json => this.setState({ list: json }));
+  }
 
-  render () {
+  render() {
     return (
-    <div className="App">
-    <Leftnav />
-      <div id="content-container">
-        <header>
-          <h2>
-            {this.state.title}
-          </h2>
-        </header>
-        {/* TODO loop through exported components and add them to the page */}
-        {/* Place Components below*/}
-        <React.Fragment>
-            <section className="component" name="cardComponent">
-            { this.state.list
-              ? this.state.list.map(item => {
-                return (<CardComponent key={item.templateId} data={item} />)})
-              : ''
-            }
-
-            </section>
-        </React.Fragment>
+      <Provider store={store}>
+        <div className="App">
+          <Leftnav />
+          <div id="content-container">
+            <header>
+              <h2>{this.state.title}</h2>
+            </header>
+            {/* TODO loop through exported components and add them to the page */}
+            {/* Place Components below*/}
+            <React.Fragment>
+              <section className="component" name="cardComponent">
+                {this.state.list
+                  ? this.state.list.map(item => {
+                      return (
+                        <CardComponent key={item.templateId} data={item} />
+                      );
+                    })
+                  : ""}
+              </section>
+            </React.Fragment>
+          </div>
         </div>
-    </div>
+      </Provider>
     );
   }
 }
