d91ffde970c06f0609f4593ca681d52bc6b0d8a0
Sabah Khan <skhan@LT-C02XD0HXJG5M.local>
2019-08-12 09:36:17 -0400

    added redux actions and reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,1 +1,9 @@
-//enter actions here
+
+export const ADD_TEMPLATE_DATA = 'ADD_TEMPLATE_DATA'
+
+export const addTemplateData = (requestHeader, requestBody, responseBody) =>({
+    type: ADD_TEMPLATE_DATA,
+    requestHeader,
+    requestBody, 
+    responseBody
+})
---
+++ client/src/reducers/templateDetails.jsx
@@ -0,0 +1,17 @@
+const templateDetails = (state= [], action) =>{
+    switch(action.type){
+        case 'ADD_TEMPLATE_DETAILS':
+            return[
+                ...state,
+                {
+                    requestHeader: action.requestHeader,
+                    requestBody: action.requestBody,
+                    responseBody: action.responseBody
+                }
+            ]
+        default:
+            return state;
+    }
+}
+
+export default templateDetails;
