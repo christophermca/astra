95b8745a5ad80df3579fd3809bb6a41deaf0aaed
Christopher MCAdams <mca.christopher@gmail.com>
2019-08-13 23:29:49 -0400

    reskinning TemplateResponse Component

--- client/src/Components/templateResponse/templateHeader.jsx
+++ client/src/Components/templateResponse/templateHeader.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+const toggle = (evt) => {
+  const el = evt.target
+  debugger
+}
+
 const TemplateHeader = (props) => {
     return(
-        <div id="template-response">
-                <label className="header-title">{props.name}</label>
-                <p className="header-info">{props.data}</p>
-        </div>
+      <div className="template-response">
+        <label className="header-title" onClick="toggle()">{props.name}</label>
+        <p className="header-info">{props.data}</p>
+      </div>
     )
 }
 
