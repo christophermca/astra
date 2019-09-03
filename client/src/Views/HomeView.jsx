f4ed1f38434459427c4cc2835aac6811e0f9e4b3
Sabah Khan <skhan@LT-C02XD0HXJG5M.local>
2019-09-03 15:10:22 -0400

    updated home screen change

--- client/src/Views/HomeView.jsx
+++ client/src/Views/HomeView.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import AssertionData from '../Components/shared/assertions/assertionData';
 
 function Home(props) {
   return (
     <React.Fragment>
-      {/* <h2> It&apos;s Astra </h2>
-      <p> '*no login required*'</p> */}
-
-      <AssertionData />
+      <h2> It&apos;s Astra </h2>
+      <p> '*no login required*'</p>
     </React.Fragment>
   );
 }
--- client/src/Views/context.js
+++ client/src/Views/context.js
@@ -1,8 +1,4 @@
 import React from 'react';
 
-<<<<<<< HEAD
 
 export const ViewContext = React.createContext({ createTempl: () => {}, templateDate: {} });
-=======
-export const ViewContext = React.createContext({ createTempl: () => {} });
->>>>>>> 61c0e0db3958264b2c5e510b35ddc6cfe3765c9c
