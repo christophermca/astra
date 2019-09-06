d64c1dfa23d5756fdc1a94fff07ccb6c54934cc1
Sabah Khan <skhan@LT-C02XD0HXJG5M.local>
2019-09-06 14:02:35 -0400

    formatting

--- client/src/Components/templateResponse/ContextVariables/contextVariables.jsx
+++ client/src/Components/templateResponse/ContextVariables/contextVariables.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const ContextVariables = (props) =>{
-    
-    
-return(
+const ContextVariables = props => {
+  return (
     <div>
-        {props.variables.map(i =>{
-            return (<>
-            <h1> {i.name }</h1>
-            <h1> {i.value }</h1>
-            </>)
-        })}
+      {props.variables.map(i => {
+        return (
+          <>
+            <h1> {i.name}</h1>
+            <h1> {i.value}</h1>
+          </>
+        );
+      })}
     </div>
-)
-}
+  );
+};
 
 export default ContextVariables;
