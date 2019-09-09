5b4229ce28fa2b41ad0b85dc25794eb1f6164afe
Sabah Khan <skhan@LT-C02XD0HXJG5M.local>
2019-09-09 13:47:22 -0400

    update context variables

--- client/src/Views/context.js
+++ client/src/Views/context.js
@@ -1,8 +1,4 @@
 import React from 'react';
 
 
-<<<<<<< HEAD
-export const ViewContext = React.createContext({ createTempl: () => {}, templateData: [], assertionPathResponse: [] });
-=======
 export const ViewContext = React.createContext({ createTempl: () => {}, saveTempl: () => {} });
->>>>>>> 4ee9c6434f780419b2dc2119e0940f99fe82662b
