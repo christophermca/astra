912e55f1fd7e8fe38988be17bc600bd19f8cee68
Christopher MCAdams <mca.christopher@gmail.com>
2019-09-12 21:28:17 -0400

    removing DetailsView

--- client/src/Components/templateResponse/DataFiles/Components/fileList.jsx
+++ client/src/Components/templateResponse/DataFiles/Components/fileList.jsx
@@ -7,7 +7,7 @@ export default (props) => {
       <input type="radio" checked={false} id={id} name='dataFile' value=""/>
       <label for={id}>
         <span>
-          {props[0].file_name}
+          {props.file_name}
         </span>
         <span>
           <a download href="/api/files/download"><img src="#" height="20" width="20" /></a>
